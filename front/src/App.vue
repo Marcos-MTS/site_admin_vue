d25b321793ddcc2fb61210dcdd5ba8555d695c73
MarcosMTS <marcosmatthies@gmail.com>
2022-01-07 06:35:44 -0300

    Inicio

---
+++ front/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+ 
+  <Topo msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Topo from './components/Topo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Topo
+  }
+}
+</script>
+
+<style>
+
+*{
+  padding: 0;
+  margin: 0;
+
+}
+
+#app {
+  font-family: "Poppins", sans-serif;
+  color: #2c3e50;
+}
+</style>
---
+++ front/src/components/Topo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="topo">
+    <h1>{{ msg }}</h1>
+  <div class="usuario">
+     <img alt="Vue logo" src="../assets/logo.png">
+    </div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Topo',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.topo{
+    height: 60px;
+    width: 100%;
+    background: #ddd;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 15px;
+    padding-right: 15px;
+    box-sizing: border-box;
+}
+
+.topo .usuario img{
+width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    -o-object-fit: cover;
+    object-fit: cover;
+    background: #ffffff;
+}
+
+</style>
